ad9729a862d7885da2fb6855ab3d43b21dd2bb76
Egor00f <velikiydolbayeb@gmail.com>
2025-12-22 23:00:17 +0500

    use system colors & upload first ref.html

--- loadContent.js
+++ loadContent.js
@@ -1,11 +1,11 @@
 $(function () {
-    $("#ref").load("data.html", function (response, status, xhr) {
+    $("#ref").load("ref.html", function (response, status, xhr) {
         if (status == "error") {
             var msg = "Sorry but there was an error: ";
             $("#ref").html(msg + xhr.status + " " + xhr.statusText);
         }
     });
-    $("#prs").load("data.html", function (response, status, xhr) {
+    $("#prs").load("prs.html", function (response, status, xhr) {
         if (status == "error") {
             var msg = "Sorry but there was an error: ";
             $("#prs").html(msg + xhr.status + " " + xhr.statusText);
